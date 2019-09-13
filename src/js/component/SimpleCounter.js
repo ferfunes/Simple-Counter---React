4d2fadc9ccd1e6d00adbdd775f16825ca06939fa
Fernando Funez <ferfunes@gmail.com>
2019-09-13 18:33:12 +0000

    added simpleCounter to ReactDOM

--- src/js/component/SimpleCounter.js
+++ src/js/component/SimpleCounter.js
@@ -3,33 +3,23 @@ import PropTypes from "prop-types";
 
 export const SimpleCounter = props => {
 	return (
-		<div className="row justify-content-center">
-			<div className="clock">
-				<i className="far fa-clock" />
+		<div className="holder">
+			<div className="row justify-content-center">
+				<div className="clock">
+					<i className="far fa-clock" />
+				</div>
+
+				<div className="sec4">{props.digitFour % 10}</div>
+
+				<div className="sec3">{props.digitThree % 10}</div>
+
+				<div className="sec2">{props.digitTwo % 10}</div>
+
+				<div className="sec1">{props.digitOne % 10}</div>
 			</div>
-
-			<div className="sec4">{props.digitFour}</div>
-
-			<div className="sec3">{props.digitThree}</div>
-
-			<div className="sec2">{props.digitTwo}</div>
-
-			<div className="sec1">{props.digitOne}</div>
 		</div>
 	);
 };
-
-var count = 0;
-setInterval(function() {
-	const sec4 = Math.floor(count / 1000);
-	const sec3 = Math.floor(count / 100);
-	const sec2 = Math.floor(count / 10);
-	const sec1 = Math.floor(count / 1);
-	console.log(sec4, sec3, sec2, sec1);
-
-	count++;
-	SimpleCounter.props.digitOne = { sec1 };
-}, 1000);
 
 SimpleCounter.propTypes = {
 	digitFour: PropTypes.number,
--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,28 @@ import "../styles/index.scss";
 
 //import your own components
 import { Home } from "./component/home.js";
+import { SimpleCounter } from "./component/SimpleCounter.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+var count = 0;
+setInterval(function() {
+	const sec4 = Math.floor(count / 1000);
+	const sec3 = Math.floor(count / 100);
+	const sec2 = Math.floor(count / 10);
+	const sec1 = Math.floor(count / 1);
+
+	count++;
+
+	ReactDOM.render(
+		<SimpleCounter
+			digitOne={sec1}
+			digitTwo={sec2}
+			digitThree={sec3}
+			digitFour={sec4}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
+
+//ReactDOM.render(<Home />, document.querySelector("#app"));
